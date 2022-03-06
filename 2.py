25b780cdd01b2672d15c7ee7c235e70e75663e89
Flad228 <100995609+Flad228@users.noreply.github.com>
2022-03-06 13:34:24 +0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,8 +1,15 @@
-spisok = []
-sum = 0
-for i in range (1000):
-    spisok.append(i**3)
-while (spisok != 0):
-    sum = sum + spisok % 10
-    spisok = spisok // 10
-print(sum)
+spisok = [x**3 for x in range(1001) if x % 2 != 0]
+print('Список кубов нечётных чисел {}'.format(spisok))
+my_spisok = spisok[:]
+sum_numbers = []
+sum_numbers_17 = []
+for i in my_spisok:
+    if sum(map(int, str(i))) % 7 == 0:
+        sum_numbers.append(i)
+print('1. Сумма чисел, делящихся на 7:', sum(sum_numbers))
+
+my_spisok = [i+17 for i in my_spisok]
+for i in my_spisok:
+    if sum(map(int, str(i))) % 7 == 0:
+        sum_numbers_17.append(i)
+print('2. Сумма чисел, делящихся на 7 (+17):', sum(sum_numbers_17))
